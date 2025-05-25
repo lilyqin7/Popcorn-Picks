11842c48e95541238c9e3562137f5b4b44c5aefa
Lily Qin <lilyqin527@gmail.com>
2025-05-24 20:19:48 -0700

    initial commit

---
+++ src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY="c064a2d969ea04b20eeb0ee591546afc";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+  export const searchMovies = async (query) => {
+    const response = await fetch(
+      `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+        query
+      )}`
+    );
+    const data = await response.json();
+    return data.results;
+  };
